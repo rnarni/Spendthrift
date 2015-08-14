7af2f9752807f0d36ef97c957a9595040b3ff427
LaxmaReddy <LaxmaReddy.Gannu@care.com>
2015-08-14 18:04:51 +0530

    welcome page

---
+++ js/welcome.js
@@ -0,0 +1,19 @@
+
+ $(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+    });
+
+
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+ $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    });
